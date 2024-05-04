31f19b72ccf1512b8853466e53cd4b96990eab0e
koola97620 <koola97620@nate.com>
2024-05-04 14:18:29 +0900

    Initial commit

---
+++ src/main/java/jadie/ticketpractice/TicketPracticeApplication.java
@@ -0,0 +1,13 @@
+package jadie.ticketpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketPracticeApplication.class, args);
+    }
+
+}
---
+++ ticket-pay/src/main/java/jadie/ticketpay/TicketPayApplication.java
@@ -0,0 +1,13 @@
+package jadie.ticketpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketPayApplication.class, args);
+	}
+
+}
