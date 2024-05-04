fe73933cd2cd58000c72a8e69dac10309f257840
koola97620 <koola97620@nate.com>
2024-05-04 14:20:49 +0900

    feat: order 모듈

---
+++ ticket-order/src/main/java/jadie/ticketorder/TicketOrderApplication.java
@@ -0,0 +1,13 @@
+package jadie.ticketorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketOrderApplication.class, args);
+    }
+
+}
