a20fbd6f3e5a0df0a3c8293b65afefc8a34b8204
koola97620 <koola97620@nate.com>
2024-05-07 22:38:06 +0900

    feat: ticket-coupon 모듈 추가

---
+++ ticket-coupon/src/main/java/jadie/ticketcoupon/TicketCouponApplication.java
@@ -0,0 +1,13 @@
+package jadie.ticketcoupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketCouponApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketCouponApplication.class, args);
+    }
+
+}
---
+++ ticket-coupon/src/test/java/jadie/ticketcoupon/TicketCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package jadie.ticketcoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
