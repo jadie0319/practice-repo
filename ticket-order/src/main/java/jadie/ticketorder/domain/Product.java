13e43444123736c545f5beaca8e9fef3b01b2f9c
koola97620 <koola97620@nate.com>
2024-06-22 21:10:31 +0900

    feat: Product 작성중(깃테스트)

---
+++ ticket-order/src/main/java/jadie/ticketorder/domain/Product.java
@@ -0,0 +1,16 @@
+package jadie.ticketorder.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long uid;
+    private String name;
+    private Integer price;
+
+}
