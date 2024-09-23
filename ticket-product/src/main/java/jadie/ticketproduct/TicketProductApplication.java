b898bbd367c823f0ca8248abf09bd92f181e48c7
Jadie <koola976@gmail.com>
2024-09-23 21:14:25 +0900

    feat: product 모듈 추가

---
+++ ticket-product/src/main/java/jadie/ticketproduct/TicketProductApplication.java
@@ -0,0 +1,13 @@
+package jadie.ticketproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketProductApplication.class, args);
+    }
+
+}
---
+++ ticket-product/src/test/java/jadie/ticketproduct/TicketProductApplicationTests.java
@@ -0,0 +1,13 @@
+package jadie.ticketproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
