cd736258258fda7782d6c03b9a1bb18585b2f710
Jadie <koola976@gmail.com>
2024-11-29 12:48:36 +0900

    feat: 프로젝트 생성

---
+++ ticket-member/src/main/kotlin/com/jadie/ticketmember/TicketMemberApplication.kt
@@ -0,0 +1,11 @@
+package com.jadie.ticketmember
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TicketMemberApplication
+
+fun main(args: Array<String>) {
+    runApplication<TicketMemberApplication>(*args)
+}
---
+++ ticket-member/src/test/kotlin/com/jadie/ticketmember/TicketMemberApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jadie.ticketmember
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TicketMemberApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
